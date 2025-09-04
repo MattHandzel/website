fbfbcc00a16e08feb1247fba19fec4ed4f865e20
MattHandzel <handzelmatthew@gmail.com>
2025-09-04 18:44:31 -0500

    whoops

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { initPostHog, isPostHogEnabled, posthog } from '../lib/posthog'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import '../styles/globals.css'
 import '../styles/heatmap.css'
 
